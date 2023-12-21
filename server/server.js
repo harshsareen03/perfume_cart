b6c7377d044779b8b3dc6e4beb81c5d2d16db9d1
harshsareen03 <harshsareen03@gmail.com>
2023-12-21 14:28:48 +0530

    user_login

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import cors from "cors";
-
+// import userRoutes from './routes/userRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 import products from "../server/data/products.js";
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
+
+
 dotenv.config();
 
 const port = process.env.PORT || 8080;
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api/products", productRoutes);
 connectDB();
@@ -25,6 +29,10 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+app.use('/api/users', userRoutes);
+
+
+
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => {
